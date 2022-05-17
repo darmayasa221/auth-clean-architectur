8e5bdbba006d5a4c584ce4269ff53f64e097cb3a
Darma Yasa <darmayasa@192.168.204.101>
2022-05-17 20:53:05 +0800

    authorization

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const authentications = require('../../Interfaces/http/api/authentications');
@@ -8,6 +9,23 @@ const createServer = async (container) => {
   const server = Hapi.Server({
     host: process.env.HOST,
     port: process.env.PORT,
+  });
+
+  await server.register({ plugin: Jwt });
+
+  server.auth.strategy('formapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
