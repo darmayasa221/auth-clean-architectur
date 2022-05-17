32e2709086487d19fb969072df1d875200128bf9
Darma Yasa <darmayasa@192.168.204.101>
2022-05-18 00:13:53 +0800

    added thread domain

---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,11 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository', () => {
+  it('should throw error when invoked abstract behavior', async () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+    // Acction and Arrange
+    await expect(threadRepository.addThread({})).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.getThreadById('')).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
